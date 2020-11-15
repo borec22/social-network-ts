3f30baee050180a588ceaa625732567b9f6eb06b
serhii <romanchuk.sss22121999@gmail.com>
2020-11-15 11:02:55 +0200

    UI - BLL Виносим даниє в сторону

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,20 +4,32 @@ import {Dialog} from './Dialog/Dialog';
 import {Message} from './Message/Message';
 
 export const Dialogs = () => {
+
+   let dialogsData = [
+      {id: 1, name: 'Andriy'},
+      {id: 2, name: 'Serhiy'},
+      {id: 3, name: 'Oleg'},
+      {id: 4, name: 'Lesya'},
+      {id: 5, name: 'Ira'},
+   ];
+
+   let messagesData = [
+      {id: 1, message: 'Lorem ipsum dolor sit.'},
+      {id: 2, message: 'Lorem ipsum is big dog cat dolor sit.'},
+      {id: 3, message: 'Lorem ipsum mouse cat house dolor sit.'},
+   ];
+
+   let dialogs = dialogsData.map(dialog => <Dialog key={dialog.id} id={dialog.id} name={dialog.name}/>);
+   const messages = messagesData.map(message => <Message key={message.id} id={message.id} message={message.message}/>)
+
    return (
       <div className={classes.dialogs}>
          <div className={classes.dialogsItems}>
-            <Dialog id={1} name='Andriy' />
-            <Dialog id={2} name='Serhiy' />
-            <Dialog id={3} name='Oleg' />
-            <Dialog id={4} name='Lesya' />
-            <Dialog id={5} name='Ira' />
+            {dialogs}
          </div>
 
          <div className={classes.messages}>
-            <Message id={1} message='Lorem ipsum dolor sit.'/>
-            <Message id={2} message='Lorem ipsum is big dog cat dolor sit.'/>
-            <Message id={3} message='Lorem ipsum mouse cat house dolor sit.'/>
+            {messages}
          </div>
       </div>
    );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,21 +3,28 @@ import classes from './MyPosts.module.css';
 import {Post} from './Post/Post';
 
 export const MyPosts = () => {
-    return (
-        <div>
-            <h3>My posts</h3>
-            <div>
-                <textarea value={'text'} placeholder='New post text...' />
-            </div>
-            <div>
-                <button>Add post</button>
-            </div>
 
-            <div className={classes.posts}>
-                <Post message='I love Ukraine.' likesCount={10} />
-                <Post message='React is the best!!!' likesCount={3} />
-            </div>
-        </div>
-    );
+   let postsData = [
+      {id: 1, message: 'I love Ukraine.', likesCount: 10},
+      {id: 2, message: 'Lorem ipsum is big dog cat dolor sit.', likesCount: 3},
+   ];
+
+   const posts = postsData.map(post => <Post key={post.id} message={post.message} likesCount={post.likesCount}/>);
+
+   return (
+      <div>
+         <h3>My posts</h3>
+         <div>
+            <textarea value={'text'} placeholder='New post text...'/>
+         </div>
+         <div>
+            <button>Add post</button>
+         </div>
+
+         <div className={classes.posts}>
+            {posts}
+         </div>
+      </div>
+   );
 }
 
